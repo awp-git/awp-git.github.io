bcfe37464dbe1422d13e5af76ea316e3e2bfac9f
JS <awpdevmain@gmail.com>
2023-05-12 22:03:18 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector(`#menu-icon`);
+let navlist = document.querySelector(`.navlist`);
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("open");
+};
+
+function redirectTo(url, newTab) {
+  if (newTab) {
+    window.open(url, "_blank");
+  } else {
+    window.location.href = url;
+  }
+}
+
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
+});
+
+sr.reveal(".hero-text", { delay: 200, origin: "top" });
+sr.reveal(".hero-img", { delay: 450, origin: "top" });
+sr.reveal(".icons", { delay: 500, origin: "left" });
+sr.reveal(".scroll-down", { delay: 200, origin: "right" });
